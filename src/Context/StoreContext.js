37afb4f035d1bc9ac6f9f9d0dd43a505b70c9fe4
Gabe <jflores353@gmail.com>
2020-03-23 12:00:38 -0500

    In StoreProvider created addProductToCart to create a new checkout. StoreContext now has addProductToCart available.

--- src/Context/StoreContext.js
+++ src/Context/StoreContext.js
@@ -1,18 +1,43 @@
 import React, { createContext, useState } from "react"
+import Client from "shopify-buy"
+
+const client = Client.buildClient({
+  domain: "advanced-gatsby.myshopify.com",
+  storefrontAccessToken: "9f3c2217037981549bd3b20a6ee0759b",
+})
 
 export const defaultValues = {
   isCartOpen: false,
   cart: [],
-  addProductToCart: () => {
-    console.log("Added to Cart!")
-  },
+  addProductToCart: () => {},
+  client,
 }
 
 export const StoreContext = createContext({ defaultValues })
 
 export const StoreProvider = ({ children }) => {
+  const addProductToCart = async variantId => {
+    try {
+      const newCheckout = await client.checkout.create()
+      const lineItems = [
+        {
+          variantId,
+          quantity: 1,
+        },
+      ]
+      const addItems = await client.checkout.addLineItems(
+        newCheckout.id,
+        lineItems
+      )
+      //* Next line will create a buy now option
+      // window.open(addItems.webUrl, "_blank")
+      console.log(addItems.webUrl)
+    } catch (e) {
+      console.error(e)
+    }
+  }
   return (
-    <StoreContext.Provider value={defaultValues}>
+    <StoreContext.Provider value={{ ...defaultValues, addProductToCart }}>
       {children}
     </StoreContext.Provider>
   )
